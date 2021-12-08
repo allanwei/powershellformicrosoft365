1eb3a9ba171064a28b899c4afc4e21be89300aa4
awei <allan.wei@hotmail.com>
2021-12-08 15:57:02 -0500

    Update Get Azure AD User.ps1

--- Get Azure AD User.ps1
+++ Get Azure AD User.ps1
@@ -1,3 +1,8 @@
 Get-AzureADUser -All $true | 
   Select-Object *,@{label="Manager";expression={(Get-AzureADUserManager -ObjectId $_.ObjectID).displayname}} | 
   Export-Csv c:\temp\file.csv
+Get-AzureADUser -All $true | 
+  Select-Object *,@{label="Manager";expression={(Get-AzureADUserManager -ObjectId $_.ObjectID).displayname}},  @{n = 'assignedlicenses1'; e = { ($_.assignedlicenses.SkuId) -join "," } },
+    @{n = 'assignedplans1'   ; e = { ($_.assignedplans.service) -join "," } }|
+  Export-Csv c:\temp\file.csv -noTypeInformation
+
